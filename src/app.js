a1efa82ca4d8243b4df0c2ecc6884ed5684db51f
Nazif Abdullah <ahmadnazif@gmail.com>
2023-11-15 21:59:24 +0800

    update app.js & add serverinfo.js model

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 const expr = require('express');
 const app = expr();
 app.use(expr.json());
+
+app.get("/info", (req, resp) => {
+
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Listened on port ${PORT}`);
+});
---
+++ src/models/serverinfo.js
@@ -0,0 +1,5 @@
+module.export = {
+    message: String,
+    version: String,
+    timeNow: Date
+}
